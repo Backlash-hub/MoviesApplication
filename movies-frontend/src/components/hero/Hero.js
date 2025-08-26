45d08cef6cb28b9a5ef38250ca43c23061c3ee17
William Pevytoe <williampevytoe@gmail.com>
2025-08-25 22:06:55 -0400

    2.08.30

--- movies-frontend/src/components/hero/Hero.js
+++ movies-frontend/src/components/hero/Hero.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import '/.Hero.css'
+import './Hero.css'
 import Carousel from 'embla-carousel-react';
+import { Paper } from '@mui/material';
 
 
-const Hero = () => {
+const Hero = ({movies}) => {
   return (
-    <div>Hero</div>
+    <div className='movie-carsousel-container'>
+      <Carousel>
+        {
+          movies.map((movie) =>{
+            return(
+              <Paper>
+                <div className='movie-carsousel-container'>
+                  <div className='movie-card'>
+                    <div className='movie-detail'>
+                      <div className='movie-poster'>
+                        <img src={movie.poster} alt='' />
+                      </div>
+                      <div className='"movie-title'>
+                        <h4>{movie.title}</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Paper>
+            )
+          })
+        }
+      </Carousel>
+    </div>
   )
 }
 
