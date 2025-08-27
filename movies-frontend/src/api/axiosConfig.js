168c4f770877aa1514b8c163f960430d3f445d82
William Pevytoe <williampevytoe@gmail.com>
2025-08-26 22:43:07 -0400

    2.17.30

--- movies-frontend/src/api/axiosConfig.js
+++ movies-frontend/src/api/axiosConfig.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://ff0f8dfc7daf.ngrok-free.app', // <-- your actual Forwarding URL
-  headers: { 'ngrok-skip-browser-warning': 'true' } // <-- REQUIRED to bypass the interstitial
+  baseURL: 'https://f5f4024a3131.ngrok-free.app',
+  headers: { 'ngrok-skip-browser-warning': 'true' }
 });
---
+++ movies-frontend/src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FrontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+
+
+const Header = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Header
